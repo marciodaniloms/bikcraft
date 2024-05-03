2e5f974ee0f04bbde7259715369804e66598174a
marciodaniloms <marciodaniloms@gmail.com>
2024-05-03 11:48:50 -0300

    galeria-imagens

--- js/script.js
+++ js/script.js
@@ -9,23 +9,25 @@ function ativarLink(link){ //função pra cada um dos links
     link.classList.add('ativo') //adiciona uma classe
   }
 }
-
 links.forEach(ativarLink); 
+
 
 //Ativar itens do orçamento
 
-const parametros = new URLSearchParams(location.search);
 
+const parametros = new URLSearchParams(location.search);
 function ativarProduto(parametro){
   const elemento = document.getElementById(parametro);
   if(elemento){
     elemento.checked = true;
   }
 }
-
 parametros.forEach(ativarProduto);
 
+
+
 //perguntas frequentes
+
 
 const perguntas = document.querySelectorAll('.perguntas button');
 
@@ -33,7 +35,6 @@ function ativarPergunta(event){
   const pergunta = event.currentTarget;
   const controls = pergunta.getAttribute("aria-controls");
   const resposta = document.getElementById(controls);
-
 
   resposta.classList.toggle("ativa");
   const ativa = resposta.classList.contains("ativa");
@@ -46,3 +47,23 @@ function eventosPerguntas(pergunta){
 }
 
 perguntas.forEach(eventosPerguntas);
+
+
+//Galeria de bicicletas
+
+const galeria = document.querySelectorAll(".bicicleta-imagens img");
+const galeriaContainer = document.querySelector(".bicicleta-imagens");
+
+function trocarImagem(event){
+  const img = event.currentTarget;
+  const media = matchMedia('(min-width:1000px)').matches;
+  if(media){
+    galeriaContainer.prepend(img);
+  }
+}
+
+function eventosGaleria(img){
+  img.addEventListener("click", trocarImagem);
+}
+
+galeria.forEach(eventosGaleria);
